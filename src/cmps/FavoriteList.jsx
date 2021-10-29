7761b919df192f20105da01a06ddf8ace0c04d95
Tal Cohen <81317982+Tal-Ivgi-Cohen@users.noreply.github.com>
2021-10-29 08:20:26 +0300

    Update FavoriteList.jsx

--- src/cmps/FavoriteList.jsx
+++ src/cmps/FavoriteList.jsx
@@ -2,15 +2,14 @@ import { Card } from '@material-ui/core'
 import { FavoritePreview } from './FavoritePreview.jsx'
 import { Loader } from './Loader.jsx'
 
-export const FavoriteList = ({ favoriteCities, onDeleteCity, onSetCity, isDarkMode, degree}) => {
+export const FavoriteList = ({ favoriteCities, onDeleteCity, onSetCity,degree}) => {
     if (!favoriteCities || favoriteCities.length === 0) return <Loader />;
     return (
         <div className='fav-list'>
             {favoriteCities.map(favoriteCity => {
                 return <Card key={favoriteCity._id} variant="outlined">
                     < FavoritePreview favoriteCity={favoriteCity}
-                        onDeleteCity={onDeleteCity} onSetCity={onSetCity}
-                        isDarkMode={isDarkMode} degree={degree} />
+                        onDeleteCity={onDeleteCity} onSetCity={onSetCity} degree={degree} />
                 </Card>
             }
             )}
